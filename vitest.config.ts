f21bd6ff35c4fb40e659925464d71a7a76b32808
Ben Meier <1651305+astromechza@users.noreply.github.com>
2025-04-29 14:50:30 +0100

    fix: increase hook timeout and repackage (#111)
    * fix: extract full error from api response
    
    * fix: update vitest.config.ts
    
    * chore: update action.ts
    
    * fix: package

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,6 @@ export default defineConfig({
   test: {
     pool: "forks", // Required because the tests use chdir
     testTimeout: 50000, // Required because the tests are building and pushing images
+    hookTimeout: 30000,
   },
 });
